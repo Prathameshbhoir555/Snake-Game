0e810865375d16b0a805b007a4a2b11aa52ce4e2
Prathameshbhoir555 <149425057+Prathameshbhoir555@users.noreply.github.com>
2024-01-23 19:03:42 +0100

    Unit Test

---
+++ test_snake.py
@@ -0,0 +1,30 @@
+import pytest
+from snake import FRUIT, MAIN, Vector2
+ 
+print("aa")
+@pytest.fixture
+def initialize_game():
+    game = MAIN()
+    return game
+
+#def test_snake_initialization():
+ #   snake = SNAKE()
+  #  assert len(snake.body) == 3
+   # assert snake.direction == Vector2(0, 0)
+    #assert snake.new_block is False
+
+def test_fruit_initialization():
+    fruit = FRUIT()
+    assert hasattr(fruit, 'pos')
+
+def test_game_update(initialize_game):
+    game = initialize_game
+    initial_snake_length = len(game.snake.body)
+    initial_fruit_position = game.fruit.pos
+
+    game.update()
+
+    assert len(game.snake.body) == initial_snake_length
+    assert game.fruit.pos != initial_fruit_position
+
+
